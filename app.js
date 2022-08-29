f826a1d9d0d9ca731b2b654587422e9f90853c7b
Marshal Olu <olmarsh.192@gmail.com>
2022-08-29 22:11:19 +0100

    added tasks file

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const express = require("express")
 const app = express()
+const tasks = require('./Routes/tasks')
 
+// Middleware
+app.use(express.json())
+
+// Routes
 app.get('/hello', (req, res) => {
     res.send('Task manager app')
 })
+
+app.use('/api/v1/tasks', tasks)
 
 
 const port = 3000
