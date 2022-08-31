6e41769f67aacf93158064cbbf00e6ca69e6d02d
Marshal Olu <olmarsh.192@gmail.com>
2022-08-31 21:16:48 +0100

    added new dependencies to fix a bug

--- Controllers/tasks.js
+++ Controllers/tasks.js
@@ -2,6 +2,27 @@ const getAllTasks = (req, res) => {
     res.send('all items')
 }
 
+const createTask = (req, res) => {
+    res.send('create task')
+}
+
+const getTask = (req, res) => {
+    res.send('get a single task')
+}
+
+const updateTask = (req, res) => {
+    res.send('update task')
+}
+
+const deleteTask = (req, res) => {
+    res.send('delete task')
+}
+
+
 module.exports = {
     getAllTasks,
+    createTask,
+    getTask,
+    updateTask,
+    deleteTask
 }
--- app.js
+++ app.js
@@ -1,18 +1,19 @@
-const express = require("express")
-const app = express()
-const tasks = require('./Routes/tasks')
+const express = require("express");
+const app = express();
+const tasks = require("./Routes/tasks").default;
 
+const port = 3000;
 // Middleware
-app.use(express.json())
+app.use(express.json());
 
 // Routes
-app.get('/hello', (req, res) => {
-    res.send('Task manager app')
+app.get("/hello", (req, res) => {
+  res.send("Task manager app");
+});
+
+app.use("/api/v1/tasks", tasks);
+
+
+app.listen(port, () => {
+    console.log(`server is listening on port ${port}...`);
 })
-
-app.use('/api/v1/tasks', tasks)
-
-
-const port = 3000
-
-app.listen(port, console.log(`server is listening on port ${port}...`))
