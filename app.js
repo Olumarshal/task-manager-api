62e5431190f9e25ad42884c0ccbeaf7370b195e5
Marshal Olu <olmarsh.192@gmail.com>
2022-09-05 23:46:56 +0100

    trying to fix a bug from the routing

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
 const express = require("express");
 const app = express();
-const tasks = require("./Routes/tasks").default;
+
+const tasks = require("./Routes/tasks");
 
 const port = 3000;
 // Middleware
-app.use(express.json());
+// app.use(express.json());
 
 // Routes
 app.get("/hello", (req, res) => {
   res.send("Task manager app");
 });
 
-app.use("/api/v1/tasks", tasks);
+// app.use("/api/v1/tasks", tasks);
 
 
 app.listen(port, () => {
